0e12a0ccac6574e6e3de438fe34284cd79d421a7
Sahilglint <sahilglint@gmail.com>
2025-08-26 18:25:41 +0530

    success

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-gray-800 mb-4 animate-pulse">
+          Coming Soon
+        </h1>
+        <p className="text-lg text-gray-600">
+          Our website is under construction. Stay tuned!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default page;
